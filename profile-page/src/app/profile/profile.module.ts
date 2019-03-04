61b727ab2889065121eda5c724d0c4d3dd6ca97d
Lucas Marques <lucas.msilva09@gmail.com>
2019-03-04 09:33:26 -0300

    Configuring the repo
    In this commit was added Jest for the project unity test and was removed
    unused component

--- profile-page/src/app/profile/profile.module.ts
+++ profile-page/src/app/profile/profile.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from "@angular/common";
 import { ProfileComponent } from "./profile.component";
 import { ProfileAvatarComponent } from "./profile-avatar/profile-avatar.component";
 import { ProfileDetailComponent } from "./profile-detail/profile-detail.component";
-import { ProfileEducationComponent } from "./profile-education/profile-education.component";
 import { ProfileExperienceComponent } from "./profile-experience/profile-experience.component";
 import { ProfileSkillsComponent } from "./profile-skills/profile-skills.component";
 
@@ -12,11 +11,10 @@ import { ProfileSkillsComponent } from "./profile-skills/profile-skills.componen
     ProfileComponent,
     ProfileAvatarComponent,
     ProfileDetailComponent,
-    ProfileEducationComponent,
     ProfileExperienceComponent,
     ProfileSkillsComponent
   ],
   imports: [CommonModule ],
-  exports: [ProfileComponent]
+  exports: [ProfileComponent, ProfileSkillsComponent]
 })
 export class ProfileModule {}
